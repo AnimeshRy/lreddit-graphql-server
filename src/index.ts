2ea427791146cc0b254f142ff2eae00c3b2d001e
AnimeshRy <ani16september@gmail.com>
2022-02-15 01:05:19 +0530

    CRUD Post with Mikro ORM Complete, Fixed Entity TypeScript Bug

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
 import { MikroORM } from '@mikro-orm/core'
 import { __prod__ } from './constants'
-import { Post } from './entities/Post'
+import express from 'express';
 import microOrmConfig from './mikro-orm.config'
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/Post';
 
 const main = async () => {
     const orm = await MikroORM.init(microOrmConfig);
     await orm.getMigrator().up()
-    // const post = orm.em.create(Post, { title: 'ez az első' } as any)
-    // await orm.em.persistAndFlush(post)
 
-    // const posts = await orm.em.find(Post, {});
-    // console.table(posts);
-}
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false // turn off class validation
+        }),
+        context: () => ({ em: orm.em })
+    })
+
+    apolloServer.applyMiddleware({ app })
+
+
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000')
+    })
+};
 
 main().catch((err) => {
     console.error(err)
