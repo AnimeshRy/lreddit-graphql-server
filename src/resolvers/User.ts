7d73d56d993b3c338e2c32f1584030527af484fe
AnimeshRy <ani16september@gmail.com>
2022-03-16 22:21:40 +0530

    Addded Email Field User Schema

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,18 +1,11 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
 import { RequiredEntityData } from "@mikro-orm/core";
-import { Resolver, Mutation, Arg, InputType, Field, Ctx, ObjectType, Query } from "type-graphql";
+import { Resolver, Mutation, Arg, Field, Ctx, ObjectType, Query } from "type-graphql";
 import argon2 from 'argon2';
 import { COOKIE_NAME } from "../constants";
-
-@InputType()
-class UsernamePasswordInput {
-    @Field()
-    username: string
-
-    @Field()
-    password: string
-}
+import { UsernamePasswordInput } from "./UsernamePasswordInput";
+import { validateRegister } from "../utils/validateRegister";
 
 @ObjectType()
 class FieldError {
@@ -34,6 +27,16 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+
+    @Mutation(() => Boolean)
+    async forgotPassword(
+        @Arg('email') email: string,
+        @Ctx() { em }: MyContext
+    ) {
+        // const user = await em.find
+        return true
+    }
+
     @Query(() => User, { nullable: true })
     async me(
         @Ctx() { req, em }: MyContext
@@ -52,26 +55,13 @@ export class UserResolver {
         @Arg('options') options: UsernamePasswordInput,
         @Ctx() { em, req }: MyContext
     ): Promise<UserResponse> {
-        if (options.username.length <= 2) {
-            return {
-                errors: [{
-                    field: "username",
-                    message: "length must be greater than 2"
-                }]
-            }
-        }
-
-        if (options.password.length <= 5) {
-            return {
-                errors: [{
-                    field: "password",
-                    message: "length must be greater than 5"
-                }]
-            }
+        const errors = validateRegister(options)
+        if (errors) {
+            return { errors }
         }
 
         const hashedPassword = await argon2.hash(options.password)
-        const user = em.create(User, { username: options.username, password: hashedPassword } as RequiredEntityData<User>)
+        const user = em.create(User, { username: options.username, password: hashedPassword, email: options.email } as RequiredEntityData<User>)
         try {
             await em.persistAndFlush(user)
         } catch (error) {
@@ -99,28 +89,20 @@ export class UserResolver {
 
     @Mutation(() => UserResponse)
     async login(
-        @Arg('options') options: UsernamePasswordInput,
+        @Arg("usernameOrEmail") usernameOrEmail: string,
+        @Arg("password") password: string,
         @Ctx() { em, req }: MyContext
     ): Promise<UserResponse> {
-        if (options.username.length <= 2) {
-            return {
-                errors: [{
-                    field: "username",
-                    message: "length must be greater than 2"
-                }]
-            }
-        }
-
-        const user = await em.findOne(User, { username: options.username })
+        const user = await em.findOne(User, usernameOrEmail.includes("@") ? { email: usernameOrEmail } : { username: usernameOrEmail })
         if (!user) {
             return {
                 errors: [{
-                    field: "username",
+                    field: "usernameOrEmail",
                     message: "that username doesn't exist"
                 }]
             }
         }
-        const valid = await argon2.verify(user.password, options.password)
+        const valid = await argon2.verify(user.password, password)
 
         // found a user ? Validate pass
         if (!valid) {
---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+
+// async..await is not allowed in global scope, must use a wrapper
+export async function sendEmail(to: string, html: string) {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'dob77do7qtsxuaze@ethereal.email', // generated ethereal user
+            pass: '7wu7TnYtZua9KSqmcU', // generated ethereal password
+        },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: to, // list of receivers
+        subject: "Change Password", // Subject line
+        html, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
