d40bbef93e974097a768aa72f2c2f8348972934a
Animesh <54435702+AnimeshRy@users.noreply.github.com>
2023-03-22 00:13:39 +0530

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,6 @@ import { Request, Response } from 'express'
 import { Redis } from 'ioredis';
 import { Session } from "express-session";
 
-
 export type MyContext = {
     em: EntityManager<IDatabaseDriver<Connection>>;
     req: Request & { session: Session };
